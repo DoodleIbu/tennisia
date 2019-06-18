e2cb5dec988817b7979b9dc2682e2774ed2b0535
kernelthree <j.v.julien@gmail.com>
2019-06-17 23:49:45 -0400

    Got something almost working

--- Ball.gd
+++ Ball.gd
@@ -2,12 +2,10 @@ extends Node2D
 
 const Renderer = preload("res://Renderer.gd")
 
-const GRAVITY = 322
 const BALL_RADIUS = 1.1
-
-# Depends on court.
 const DAMPING = 0.5
 
+var gravity = -322 # This will change according to spin and bounce.
 var actual_position = Vector3(100, -80, 400)
 var velocity = Vector3(0, 0, 0)
 
@@ -21,12 +19,32 @@ func _process(delta):
 # Fire shot from the other side of the court.
 func fire():
     actual_position = Vector3(180, -40, 0)
-    velocity = Vector3(rand_range(-100, 100), rand_range(-160, -80), rand_range(600, 800))
 
-func _physics_process(delta):
-    if Input.is_action_just_pressed("ui_accept"):
-        fire()
+    var goal = Vector3(0, 0, 780)
 
+    # Get the distance to cover in the xz-plane.
+    var distance_to_cover = Vector2(goal.x, goal.z).distance_to(Vector2(actual_position.x, actual_position.z))
+
+    print(distance_to_cover)
+    var desired_peak = -100 # Depends on character and shot type.
+    var power = 500
+
+    gravity = -4 * (actual_position.y - 2 * desired_peak - BALL_RADIUS) * pow(power, 2) / pow(distance_to_cover, 2)
+    print(gravity)
+    var v_y = -1 * (3 * actual_position.y - 4 * desired_peak - BALL_RADIUS) * power / distance_to_cover
+
+    print(v_y)
+
+    var direction_vector = Vector2(goal.x - actual_position.x, goal.z - actual_position.z).normalized()
+    print(direction_vector)
+
+    # velocity in the xz plane
+    var twod_velocity = Vector2(power, v_y)
+
+    # Rotate the vector...
+    velocity = Vector3(power * direction_vector.x, v_y, power * direction_vector.y)
+
+func update_position_and_velocity(delta):
     # Detect collision on floor...
     var new_position = actual_position + velocity * delta
 
@@ -36,9 +54,17 @@ func _physics_process(delta):
         var time_percent_after_bounce = 1 - time_percent_before_bounce
 
         var position_at_bounce = actual_position + time_percent_before_bounce * velocity * delta
+        print(position_at_bounce)
         velocity = velocity.bounce(Vector3(0, -1, 0)) * DAMPING # Normal and damping is different per court.
         actual_position = position_at_bounce + time_percent_after_bounce * velocity * delta
     else:
         actual_position += velocity * delta
 
-    velocity.y += GRAVITY * delta
+    velocity.y -= gravity * delta
+
+func _physics_process(delta):
+    if Input.is_action_just_pressed("ui_accept"):
+        fire()
+
+    update_position_and_velocity(delta)
+
